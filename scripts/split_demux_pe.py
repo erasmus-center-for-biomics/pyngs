39c0d178bb40d6b0c9da5ada0af3406eeedff105
Rutger Brouwer <r.w.w.brouwer@erasmusmc.nl>
2018-07-24 15:52:26 +0200

    added a script to split demux tables
    Signed-off-by: Rutger Brouwer <r.w.w.brouwer@erasmusmc.nl>

---
+++ scripts/split_demux_pe.py
@@ -0,0 +1,49 @@
+#!/bin/env python3
+
+import sys
+import argparse
+
+if __name__ == "__main__":
+
+    def main():
+        """Run the main program loop."""
+        parser = argparse.ArgumentParser(
+            prog=sys.argv[0],
+            description="""A script to demultiplex PE demux files.""")
+        parser.add_argument(
+            "-i", "--input", dest="int",
+            type=str, nargs="?", default="stdin",
+            help="The input table.")
+        parser.add_argument(
+            "-p", "--prefix", dest="prefix",
+            type=str,
+            help="The prefix of the output files.")
+        # parse the command line parameters
+        args = parser.parse_args()
+
+        # open the in and output streams for regular and compressed files
+        instream = open(args.input, "r") if args.input != "stdin" else sys.stdin
+
+        handles = {}
+        for line in instream:
+            line = line.rstrip()
+            tokens = line.split("\t") 
+
+            if len(tokens) < 9:
+                continue
+
+            out = tokens[6]
+            if out not in handles.keys():
+                handles[out] = (
+                    open(prefix + out + "_R1.fastq", "wt"),
+                    open(prefix + out + "_R2.fastq", "wt"))
+            data = handles[out]
+            data[0].write("@{0}\n{1}\n+\n{2}\n".format(tokens[0], tokens[1], tokens[2]))
+            data[1].write("@{0}\n{1}\n+\n{2}\n".format(tokens[3], tokens[4], tokens[5]))
+        
+        for data in handles.items():
+            data[0].close()
+            data[1].close()
+            
+    # run the program
+    main()
